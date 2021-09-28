8870add507d9e3365d7c31ed09a7a4b554fb09ab
yinghua-hu <yinghua.hu@gmail.com>
2021-09-28 10:48:05 -0700

    Create LC922.java

---
+++ LC922.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int j = 1;
+        while (i < n && j < n) {
+            while (i < n && nums[i] % 2 == 0) {
+                i += 2;
+            }
+            while (j < n && nums[j] % 2 == 1) {
+                j += 2;
+            }
+            if (i < n && j < n) {
+                swap(nums, i, j);    
+            }            
+        }
+        return nums;        
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
