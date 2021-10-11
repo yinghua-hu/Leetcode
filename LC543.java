57cde89d077c05666a2455e1f39cb53e30b76c42
yinghua-hu <yinghua.hu@gmail.com>
2021-10-11 07:14:55 -0700

    Create LC543.java

---
+++ LC543.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 0;
+        maxDepth(root);
+        return ans;
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        ans = Math.max(ans, l + r);
+        return Math.max(l, r) + 1;
+    }
+}
+
+/*
+time O(N) 
+space O(N) O(H)
+
+*/
